2890ea73359849c51689068bfd650f05342363f2
heacol2230 <heacol2230@students.ecpi.edu>
2024-03-19 19:01:48 -0400

    changes per input from professor 3/19/24

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import ReactDOM from "react-dom";
+import "./index.css";
 import Main from "./Main";
 
 // Replace ReactDOM.render with createRoot
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+  root.render( <React.StrictMode> 
+    <Main /> </React.StrictMode> );
